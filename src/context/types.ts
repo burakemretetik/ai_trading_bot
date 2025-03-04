3c434cec54239222704f27e2576491898bcbf550
Burak Tetik <burakemretetik@gmail.com>
2025-03-05 00:50:32 +0300

    Delete src directory

--- src/context/types.ts
+++
@@ -1,9 +0,0 @@
-
-export type UserContextType = {
-  isLoading: boolean;
-  phone: string | null;
-  isAuthenticated: boolean;
-  signInWithPhone: (phone: string) => Promise<void>;
-  verifyOTP: (phone: string, token: string) => Promise<void>;
-  signOut: () => Promise<void>;
-};
